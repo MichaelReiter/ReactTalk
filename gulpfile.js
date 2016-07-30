babdd198c31fd5b107030830ee5ea84d3b4cb9fe
MichaelReiter <michaelreiter@live.ca>
2016-07-30 15:05:48 -0400

    Add livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-const gulp   = require('gulp');
-const babel  = require('gulp-babel');
-const stylus = require('gulp-stylus');
+const gulp       = require('gulp');
+const babel      = require('gulp-babel');
+const stylus     = require('gulp-stylus');
+const livereload = require('gulp-livereload');
 
 const htmlSource   = './src/html/*.html';
-const stylusSource = './src/styles/main.styl';
+const stylusSource = './src/styles/*.styl';
 const jsSource     = './src/js/*.js';
 const assetsSource = './assets/*';
 const destination  = './build';
@@ -14,26 +15,30 @@ gulp.task('js', () => {
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest(destination));
+    .pipe(gulp.dest(destination))
+    .pipe(livereload());
 });
 
 // Build Stylus stylesheets
 gulp.task('styl', () => {
   return gulp.src(stylusSource)
     .pipe(stylus())
-    .pipe(gulp.dest(destination));
+    .pipe(gulp.dest(destination))
+    .pipe(livereload());
 });
 
 // Copy markup
 gulp.task('html', () => {
   return gulp.src(htmlSource)
-    .pipe(gulp.dest(destination));
+    .pipe(gulp.dest(destination))
+    .pipe(livereload());
 });
 
 // Copy assets
 gulp.task('assets', () => {
   return gulp.src(assetsSource)
-    .pipe(gulp.dest(destination));
+    .pipe(gulp.dest(destination))
+    .pipe(livereload());
 });
 
 // Build everything and watch for changes
@@ -43,6 +48,7 @@ gulp.task('default', [
     'js',
     'assets'
   ], () => {
+    livereload.listen();
     gulp.watch(htmlSource, ['html']);
     gulp.watch(jsSource, ['js']);
     gulp.watch(stylusSource, ['styl']);
