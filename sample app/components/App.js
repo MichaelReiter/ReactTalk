355a80ad040892ef6e73631786816ae84b8c53b8
MichaelReiter <michaelreiter@live.ca>
2016-07-30 17:24:13 -0400

    Add initial sample app files

---
+++ sample app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+});
+
+export default App;
